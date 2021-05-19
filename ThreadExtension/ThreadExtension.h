ada39259ed6ce99931f808388802d1b6afd5bf5d
Martin Jacob Rehder <gitrepocon01@rehsco.com>
2021-05-19 13:18:46 +0200

    Initial Commit

---
+++ ThreadExtension/ThreadExtension.h
@@ -0,0 +1,18 @@
+//
+//  ThreadExtension.h
+//  ThreadExtension
+//
+//  Created by Martin Rehder on 19.05.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ThreadExtension.
+FOUNDATION_EXPORT double ThreadExtensionVersionNumber;
+
+//! Project version string for ThreadExtension.
+FOUNDATION_EXPORT const unsigned char ThreadExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ThreadExtension/PublicHeader.h>
+
+
